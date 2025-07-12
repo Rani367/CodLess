3af637abda7db6cee7e8596df08917cf30b25046
Rani Malach <ranimac@Mac.lan>
2025-07-12 22:08:39 +0300

    Add C++ implementation with Qt6 GUI

---
+++ cpp/build/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_CodLess_FILE /Users/ranimac/Documents/NNBMP/cpp/build/CodLess.app/Contents/MacOS/CodLess)
+set(__QT_DEPLOY_TARGET_CodLess_TYPE EXECUTABLE)
---
+++ cpp/build/CMakeFiles/CodLess_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CodLess_autogen"
+  "CodLess_autogen/mocs_compilation.cpp"
+  "CodLess_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/CodLess_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
